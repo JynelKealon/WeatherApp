ebf31d0390c557a4030861db89631581ab16ce4f
Jynel Kealon <jkealon13@r2happrentice.org>
2023-06-12 16:00:36 -0400

    new changes

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-// Configure the Express app
+// Configure the Express app to handle form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve index.html file for the root route
+// Serve the main index.html file for the root route
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Serve static files from the "WeatherApp" folder
+// Display and use the existing files like HTML, CSS, and JavaScript that are stored in the "WeatherApp" folder as they are without changing them.
 app.use(express.static("WeatherApp"));
 
-// Serve index.html file for the root route within "WeatherApp" folder
+// Serve the index.html file within the "WeatherApp" folder
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/WeatherApp/index.html");
 });
@@ -21,13 +21,14 @@ app.get("/", function (req, res) {
 // Handle POST request to the root route
 app.post("/", function (req, res) {
   const input = req.body.cityName;
-  const isNumericInput = !isNaN(input); // Check if the input contains only numbers
+  const isNumericInput = !isNaN(input); // Check if the input is numeric
   let lat;
   let lon;
 
   if (isNumericInput) {
     // Use zip code API if the input is numeric
     const dataUrl = `https://api.openweathermap.org/geo/1.0/zip?zip=${input},US&appid=511d8dc691c3f462fc7290f379695d0c`;
+   // Send a request to get latitude and longitude data for a location, and then use the received data to fetch weather information for that location.
     https.get(dataUrl, function (response) {
       response.on("data", function (data) {
         const jsonData = JSON.parse(data);
@@ -39,6 +40,7 @@ app.post("/", function (req, res) {
   } else {
     // Use city name API if the input is a string
     const dataUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${input},US&appid=511d8dc691c3f462fc7290f379695d0c`;
+    // Send a request to get latitude and longitude data for a location (using city name), and then use the received data to fetch weather information for that location.
     https.get(dataUrl, function (response) {
       response.on("data", function (data) {
         const jsonData = JSON.parse(data);
@@ -50,6 +52,25 @@ app.post("/", function (req, res) {
   }
 });
 
-// Function to fetch weather data
+// Function to fetch weather data and send it as a response
 function fetchWeatherData(res, lat, lon, cityName) {
-  const url = `https://api.openweathermap.org/data/2.
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=511d8dc691c3f462fc7290f379695d0c&units=imperial`;
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      const jsonData = JSON.parse(data);
+      const temp = jsonData.main.temp;
+      const des = jsonData.weather[0].description;
+      const icon = jsonData.weather[0].icon;
+      const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+      // Send the weather information as a response
+      res.write(`<h1>The temperature in ${cityName} is ${temp} degrees</h1>`);
+      res.write(`<p>The weather description is ${des} </p>`);
+      res.write(`<img src="${imageUrl}">`);
+      res.send();
+    });
+  });
+}
+
+// Start the server and listen on port 9000
+app.listen(9000);
